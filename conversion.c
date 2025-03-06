48bb97b1a2655122b9a10e80dbe92ca3bdd2380f
SurabhiIyengar <156432043+SurabhiIyengar@users.noreply.github.com>
2025-03-06 14:27:52 +0530

    Add files via upload

---
+++ conversion.c
@@ -0,0 +1,17 @@
+//
+//#include <stdio.h>
+//
+//
+//char alp() {
+//    int x;
+//    printf("Enter the integer: ");
+//    scanf("%d", &x);
+//    return x;
+//}
+//
+//
+//int main() {
+//    printf("%c", alp());
+//    return 0;
+//}
+//
