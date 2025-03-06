ae130c052c6db9ea8c5870a54d57a5c52e1bf849
SurabhiIyengar <156432043+SurabhiIyengar@users.noreply.github.com>
2025-03-06 14:29:19 +0530

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -1,17 +1,17 @@
-//
-//#include <stdio.h>
-//
-//
-//char alp() {
-//    int x;
-//    printf("Enter the integer: ");
-//    scanf("%d", &x);
-//    return x;
-//}
-//
-//
-//int main() {
-//    printf("%c", alp());
-//    return 0;
-//}
-//
+
+#include <stdio.h>
+
+
+char alp() {
+    int x;
+    printf("Enter the integer: ");
+    scanf("%d", &x);
+    return x;
+}
+
+
+int main() {
+    printf("%c", alp());
+    return 0;
+}
+
