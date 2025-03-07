6be058fd5e76e258bfbb5c52bb89664c5132c139
SurabhiIyengar <156432043+SurabhiIyengar@users.noreply.github.com>
2025-03-07 14:38:04 +0530

    Add files via upload

---
+++ Recursion.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int add(int num){
+    if(num<=0){
+        return 0;
+    }
+    if(num%2==0){
+        return add(num-1);
+    }
+    
+    return num + add(num-1);
+}
+int main(){
+    printf("%d",add(5));
+}
---
+++ addition (function).c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int add(int a,int b){
+return a+b;
+}
+int main()
+{
+printf("the sum of a and b is:%d\n",add(5,2));
+return 0;
+}
+
+ 
+   
